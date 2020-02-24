3bed774f8bb2d98a08d0fdd259c7bb380b9869ab
Rusty <rww6@sfu.ca>
2020-02-24 13:00:03 -0800

    Added Crosshair

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// Enemy Class
+public class test : MonoBehaviour
+{
+    public float health = 5f;
+    public NavMeshAgent navAgent; 
+    public GameObject target;
+
+    void start(){
+        navAgent = GetComponent<NavMeshAgent>(); 
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+    void update(){
+        navAgent.destination = target.transform.position;
+    }
+
+    // COLLISION CHECK
+    void OnTriggerEnter2D(Collider2D collision){
+        
+        // CHECK COLLISION WITH PROJECTILE
+        if(collision.gameObject.tag == "Projectile"){
+            // Destroy the object that collides with enemy
+            Destroy(gameObject);
+            // Decrease enemy health by 1 
+            health = health - 1;
+        }
+
+        // CHECK COLLISION WITH PLAYER 
+        
+
+
+
+    }
+
+
+}
